b9a09c53bc6971af42ad16eb64272af93b40cda9
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-10 22:59:32 +0200

    Moore css and moore cleanup

--- frontend/src/components/SiteHeader.jsx
+++ frontend/src/components/SiteHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './SiteHeader.css'
 
 export default function SiteHeader() {
   return (
     <div className='side-header'>
-        <Link to="/">Game Reviews</Link>
+        <Link to="/"><h1>Game Reviews</h1></Link>
     </div>
   )
 }
