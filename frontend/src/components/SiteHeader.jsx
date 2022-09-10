6423467a4440cb751896f7025ce1016c49ea42e0
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-10 23:09:15 +0200

    Added my name to it

--- frontend/src/components/SiteHeader.jsx
+++ frontend/src/components/SiteHeader.jsx
@@ -5,7 +5,7 @@ import './SiteHeader.css'
 export default function SiteHeader() {
   return (
     <div className='side-header'>
-        <Link to="/"><h1>Game Reviews</h1></Link>
+        <Link to="/"><h1>Visceral Game Reviews</h1></Link>
     </div>
   )
 }
