138f726e2a86a2ff06884f2e93a782c3e15c7820
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-10 22:00:58 +0200

    installed the router

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function header() {
+  return (
+    <div className='side-header'>
+        <Link to="/">Game Reviews</Link>
+    </div>
+  )
+}
