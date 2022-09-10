16c49a112e178d8cee362e341f40962a0de7cd75
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-10 22:05:09 +0200

    importing pages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Pages
+import home from './pages/home';
+import reviewdetails from './pages/reviewdetails';
+import category from './pages/category';
+
+// Components
+import header from './components/header';
+
+
 
 function App() {
   return (
