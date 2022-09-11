7e98f96053408673e9742e5b0111acaa3555cf1c
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-11 23:07:35 +0200

    Thank god for youtube comments!

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -6,7 +6,26 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    
+    useEffect( () => {
+        const fetchData = async () => {
+            setLoading(true)
+
+            try {
+                const res = await fetch(url)
+                const json = await res.json()
+
+                setData(json)
+                setLoading(false)
+            } catch(error) {
+                setError(error)
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [url])
+
+    return { loading, error, data}
 }
 
 export default useFetch
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
+//import useFetch from '../hooks/useFetch'
 
 
 // Constructing the QUERY
@@ -9,15 +10,16 @@ const REVIEWS = gql`
   query GetReviews {
     reviews {
       data {
-        id, attributes {
-          title,
-          rating,
-          body,
+        id
+        attributes {
+          title
+          rating
+          body
         }
       }
     }
   }
-`
+`;
 
 export default function HomePage() {
 
@@ -27,12 +29,25 @@ const { loading, error, data } = useQuery(REVIEWS)
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error</p>
 
-  console.log(data)
-
   return (
     <div>
-      {data.reviews.map(review => (
-        <div key={review.id} className="review-card">
+      {data.reviews.data.map((review) => (
+        <div key={review.attributes.id} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+          <small>console list</small>
+          <p>{review.attributes.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+  /* OLD RETURN !!!
+  return (
+    <div>
+      {data.data.map(review => (
+        <div key={data.data.id} className="review-card">
           <div className="rating">{review.rating}</div>
           <h2>{review.title}</h2>
 
@@ -44,3 +59,5 @@ const { loading, error, data } = useQuery(REVIEWS)
     </div>
   )
 }
+
+*/ 
