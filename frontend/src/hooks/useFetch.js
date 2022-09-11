70630ee8600e8778b82620771c213813da42056c
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-11 14:25:59 +0200

    Im gonna be honest, no idea what this does.

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+}
+
+export default useFetch
