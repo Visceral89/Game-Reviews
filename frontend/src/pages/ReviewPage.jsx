7d795c400945c24132217f5d362c66e47f37565d
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-12 00:00:42 +0200

    FINALLY
    Got it working ! Same problem as before.
    data.review.data.attributes saved me!

--- frontend/src/pages/ReviewPage.jsx
+++ frontend/src/pages/ReviewPage.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+
+const REVIEW = gql`
+  query GetReview($id:ID!) {
+    review(id:$id) {
+      data {
+        id
+        attributes {
+          title,
+          rating,
+          body
+        }
+      }
+    }
+  }
+`;
 
 export default function ReviewPage() {
+
+  const { id } = useParams()
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id: id }
+  })
+  
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+  
+  console.log(data)
+
   return (
-    <div>ReviewPage</div>
+    <div className="review-card">
+      <div className="rating">{data.review.data.attributes.rating}</div>
+      <h2>{data.review.data.attributes.title}</h2>
+
+      <small>console list</small>
+
+      <p>{data.review.data.attributes.body}</p>
+    </div>
   )
 }
+
